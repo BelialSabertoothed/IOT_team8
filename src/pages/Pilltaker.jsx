42d50b3f6162ec28ede39e684a396b6f700fb81c
Zuzana Fialová <zuza.fialka@seznam.cz>
2024-03-28 16:16:16 +0100

    Pilltaker placeholder

--- src/pages/Pilltaker.jsx
+++ src/pages/Pilltaker.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 
+import {
+  Grid, Box, Image, Text, Title
+} from '@mantine/core';
+
 function Pilltaker() {
   return (
-    <div>Pilltaker</div>
+    <Grid justify="center" align="flex-start">
+      <Grid.Col span={{ base: 11,xxs: 11, xs: 11, sm: 6, md: 6, lg: 6 }}>
+      <Box>
+        <Image
+          radius={100}
+          src={null}
+          h={200}
+          w={200}
+          fit="cover"
+          fallbackSrc="https://placehold.co/600x400?text=Placeholder"
+        />
+        <Title order={1}>Namme of user</Title>
+        <Text c="violet">
+          Basic informations, I dont know what information. Thats why placeholder. BackgroundImage component can be used to add any content on image. It is useful for hero
+          headers and other similar sections
+        </Text>
+    </Box>
+    </Grid.Col>
+    </Grid>
   )
 }
 
