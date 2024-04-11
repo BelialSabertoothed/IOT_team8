9940f39acd15e08463d4d32a32aa26829935c335
Zuzana Fialová <zuza.fialka@seznam.cz>
2024-04-11 17:53:22 +0200

    gdgd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import useFetch from "../useFetch";
 import CreateMedsTaker from '../components/MedsTaker/createMedsTaker';
-import { Loader, Card, Grid, Text, Avatar} from '@mantine/core';
+import { Loader, Card, Grid, Text, Avatar, Box, Title, Button} from '@mantine/core';
 
 function Home() {
   const [MedsTakers, MedsTakersPending, MedsTakersError, MedsTakersRefresh] = useFetch('http://localhost:3001/medsTaker/list');
@@ -12,22 +12,28 @@ function Home() {
   
   const addMedsTakerCart = <CreateMedsTaker refreshData={refreshData}></CreateMedsTaker>
   const medsTakersCarts = MedsTakers?.map((MedsTaker) => (
-    <Card w='200' h='100' key={MedsTaker._id} withBorder={true} shadow="sm" component="a" href={"/pilltaker?"+MedsTaker._id}>
-      <Grid justify="center" align="flex-start">
-        <Grid.Col span={4}><Avatar></Avatar></Grid.Col>
-        <Grid.Col span={8}><Text>{MedsTaker.name}</Text></Grid.Col>
-        <Grid.Col span={12}><Text>+{MedsTaker.phone_country_code} {MedsTaker.phone_number}</Text></Grid.Col>
+    <Card w='290' h='200' mt={20} key={MedsTaker._id} withBorder={true} shadow="sm" component="a" href={"/pilltaker?"+MedsTaker._id}>
+      <Grid justify="center">
+        <Grid.Col span={4}><Avatar size="xl"/></Grid.Col>
+        <Grid.Col span={8}><Title>{MedsTaker.name}</Title></Grid.Col>
+        {/* <Grid.Col span={12}><Text>+{MedsTaker.phone_country_code} {MedsTaker.phone_number}</Text></Grid.Col> */}
       </Grid> 
     </Card>
   ));
 
   if (MedsTakersPending) {return <Loader size={30} />;}
   else if (MedsTakersError) {return (<div>ERROR</div>)}
-  else return (      
-    <Grid justify="center" align="flex-start">
-      {medsTakersCarts}
-      {addMedsTakerCart}
-    </Grid>   
+  else return (  
+    <Box maw={{ base: 300, xxs: 300, xs: 300, sm: 600, md: 900, lg: 900, xl: 900}} mx="auto" mt={50}>
+      <Box mb={10}>
+        {addMedsTakerCart}
+      </Box>
+      <Grid justify="space-between" align="flex-start">
+        {medsTakersCarts}
+        <Card /* prosim nemazat */  w='290' h='200' mt={20}/>
+        
+      </Grid>
+    </Box>   
   )
 }
 
