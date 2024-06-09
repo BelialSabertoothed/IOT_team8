fefc13e72287ae3cd6c38f7aa224162ceed483de
Zuzana Fialová <zuza.fialka@seznam.cz>
2024-06-09 18:20:17 +0200

    modallogin open after time

--- src/components/Login/modalLogin.jsx
+++ src/components/Login/modalLogin.jsx
@@ -1,17 +1,28 @@
-import {React} from 'react'
+import {React, useState} from 'react'
 import {useMantineTheme, UnstyledButton, Modal } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import LoginForm from './loginForm';
 
 function ModalLogin() {
-  const [opened, { open, close }] = useDisclosure(true);
+  const [opened, { open, close }] = useDisclosure(false);
   const theme = useMantineTheme();
+  let [firstTime,setFirstTime] = useState(true)
+  
+  if (firstTime === true) {
+    window.onload = setTimeout(() => {
+          open()
+        }, 2000);
+    setFirstTime(false)
+    } else {null}
+  
    return (
+    
     <>
-    <Modal size={'lg'} opened={opened} onClose={close} title="Please Log in" centered>
+    <Modal size={'lg'} opened={opened} onClose={close} title="Please Log in" centered transitionProps={{ transition: 'fade', duration: 500 }} >
       <LoginForm modal={true}/>
     </Modal>
-    <UnstyledButton style={{color: theme.colors.violet[5]}} onClick={open}>Log in</UnstyledButton>
+    <UnstyledButton style={{color: theme.colors.violet[5]}} onClick={() => {open()}}
+        >Log in</UnstyledButton>
     </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -73,7 +73,7 @@ function Home() {
 
   if (MedsTakersPending) {return <Box maw={{ base: 30, xxs: 30, xs: 30, sm: 30, md: 30, lg: 30, xl: 30}} mx="auto" mt={50}><Loader size={30} color='vioet' /></Box>;}
   else if (MedsTakersError) {return (
-  <Box maw={{ base: 300, xxs: 300, xs: 300, sm: 500, md: 500, lg: 500, xl: 500}} mx="auto" mt={80}>
+  <Box maw={{ base: 300, xxs: 300, xs: 300, sm: 500, md: 500, lg: 500, xl: 500}} mx="auto" mt={80} align='center'>
     <Image
       h={{ base: 272, xxs: 272, xs: 272, sm: 450, md: 450, lg: 450, xl: 450}}
       w={{ base: 300, xxs: 300, xs: 300, sm: 500, md: 500, lg: 500, xl: 500}}
@@ -81,7 +81,7 @@ function Home() {
       radius={10}
       fallbackSrc='https://i.pinimg.com/originals/5a/e4/9a/5ae49a02fc6d1281dd9267d679e6ba01.jpg'
       />
-      <Text>{errorMessage} please <ModalLogin/></Text>
+      <Text >{errorMessage} please <ModalLogin/></Text>
       
   </Box>
   )}
