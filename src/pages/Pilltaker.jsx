e2ef8e308aa2dd22cc83825f486ce5b7495846f6
Philip Ernst <98410174@cuni.cz>
2024-06-11 10:53:02 +0200

    Pill groups

--- src/pages/Pilltaker.jsx
+++ src/pages/Pilltaker.jsx
@@ -32,7 +32,6 @@ function Pilltaker() {
     errorMessage: errorMessage,
     refetch: MedsTakersRefresh
   } = useAxiosFetch(`/medsTaker/get` + medsTakerID);
-
   const {
     isLoading: UnitsPending,
     data: Units,
@@ -41,8 +40,7 @@ function Pilltaker() {
     errorMessage: UnitsErrorMessage,
     refetch: UnitsRefresh
   } = useAxiosFetch(`/unit/list`);
-
-  const [takenStates, setTakenStates] = useState({});
+  const MedicineCanBeTaken = []   //TODO get from server
   const [groupedMedicines, setGroupedMedicines] = useState([]);
   const [modalOpened, setModalOpened] = useState(false);            /*Neded*/
   const [selectedMedicine, setSelectedMedicine] = useState(null);   /*Neded*/
@@ -50,38 +48,10 @@ function Pilltaker() {
   useEffect(() => {
     if (Medicine) {
       console.log("Medicine data loaded:", Medicine);
-      const initialState = {};
-      Medicine.forEach((medicine) => {
-        initialState[medicine._id] = false; // Default state is "Not Taken"
-      });
-      setTakenStates(initialState);
-      const filteredAndGrouped = groupMedicinesByTime(filterMedicinesInNext48Hours(Medicine));
+      const filteredAndGrouped = MedicinesInNext24Hours(MedicineCanBeTaken,Medicine);
       setGroupedMedicines(filteredAndGrouped);
     }
   }, [Medicine]);
-
-  const handleButtonClick = (id, dose) => {
-    console.log(`Button clicked for medicine ID: ${id}`);
-    setTakenStates(prevState => {
-      const newState = !prevState[id];
-
-      const updatedMedicines = Medicine.map(medicine => {
-        if (medicine._id === id && newState && !prevState[id]) {
-          medicine.history.push({
-            startDate: new Date().toISOString(),
-            endDate: new Date().toISOString(),
-            state: "Active"
-          });
-          medicine.count = Math.max(0, medicine.count - dose);
-        }
-        return medicine;
-      });
-
-      const newGroupedMedicines = groupMedicinesByTime(filterMedicinesInNext48Hours(updatedMedicines));
-      setGroupedMedicines(newGroupedMedicines);
-      return { ...prevState, [id]: newState };
-    });
-  };
 
   const handlePillClick = (medicine) => {     /*Neded*/
     setSelectedMedicine(medicine);
@@ -165,115 +135,62 @@ function Pilltaker() {
     //TODO 
   }
 
-  const getNextDoseTime = (medicine) => {
+  const MedicinesInNext24Hours = (MedicineCanBeTaken, medicines) => {
     const now = new Date();
-    const currentDay = now.getDay();
-    let nextDose = null;
-
-    medicine.reminder.forEach(reminder => {
-      const { days, hours, minutes } = getReminderTimes(reminder);
-
-      days.forEach(day => {
-        hours.forEach((hour, idx) => {
-          const minute = minutes[idx];
-          const reminderTime = new Date(now);
-          reminderTime.setHours(hour, minute, 0, 0);
-
-          if (day === currentDay && reminderTime > now) {
-            // Reminder is later today
-            if (nextDose === null || reminderTime < nextDose) {
-              nextDose = reminderTime;
-            }
-          } else if (day > currentDay) {
-            // Reminder is later this week
-            reminderTime.setDate(now.getDate() + (day - currentDay));
-            if (nextDose === null || reminderTime < nextDose) {
-              nextDose = reminderTime;
-            }
-          } else if (day < currentDay) {
-            // Reminder is next week
-            reminderTime.setDate(now.getDate() + (7 - (currentDay - day)));
-            if (nextDose === null || reminderTime < nextDose) {
-              nextDose = reminderTime;
-            }
-          }
-        });
-      });
-    });
-
-    return nextDose;
-  };
-
-  const filterMedicinesInNext48Hours = (medicines) => {
-    const now = new Date();
-    const in48Hours = new Date(now.getTime() + 48 * 60 * 60 * 1000); // 48 hours from now
-
-    const filteredMedicines = medicines.filter(medicine => {
-      const nextDoseTime = getNextDoseTime(medicine);
-      const isInNext48Hours = nextDoseTime >= now && nextDoseTime <= in48Hours;
-      return isInNext48Hours;
-    });
-
-    return filteredMedicines;
-  };
-
-  const groupMedicinesByTime = (medicines) => {
-    const grouped = {};
-
-    medicines.forEach((medicine) => {
-      const nextDoseTime = getNextDoseTime(medicine);
-      if (!nextDoseTime) return; // Skip if nextDoseTime is null
-      const timeKey = nextDoseTime.toISOString().slice(0, 16); // Use date and hour:minute as key
-      if (!grouped[timeKey]) {
-        grouped[timeKey] = [];
+    const in24Hours = new Date(
+      Date.UTC(now.getFullYear(), now.getMonth(), now.getDate() + 1, 
+      now.getHours(), now.getMinutes(), 0)) // 24 hours from now
+    let nextMedicineIn24hour = []
+    let usedDose = []
+    MedicineCanBeTaken.forEach(item => {
+      const medicine = medicines.find((medicine) => medicine._id == item.id)
+      const usedDoseIndex = usedDose.indexOf((dose) => dose.id == item.id)
+      if (usedDoseIndex == -1) {
+        usedDose.push({id:item.id, dose:item.dose})
+        nextMedicineIn24hour.push({medicine:medicine, time:now, dose:item.dose, dosesUntilNow:item.dose, canBeTaken:true})
       }
-      grouped[timeKey].push({ ...medicine, time: nextDoseTime });
+      else {
+        usedDose[usedDoseIndex].dose += item.dose
+        nextMedicineIn24hour.push({medicine:medicine, time:now, dose:item.dose, dosesUntilNow:usedDose[usedDoseIndex].dose, canBeTaken:true})
+      }
+    })
+    console.log("time 24:",in24Hours)
+    medicines.forEach(medicin => {
+      const nextMedicine = getNextMedicineTime(medicin.reminder)
+      console.log("nextMedicine:",nextMedicine)
+      nextMedicine.forEach(item => {
+        if(item.time.valueOf() < in24Hours.valueOf()) {
+          const usedDoseIndex = usedDose.map(e => e.id).indexOf(medicin._id)
+
+          if (usedDoseIndex == -1) {
+            usedDose.push({id:medicin._id, dose:item.dose})
+            nextMedicineIn24hour.push({medicine:medicin, time:item.time, dose:item.dose, dosesUntilNow:item.dose, canBeTaken:false})
+          }
+          else {
+            usedDose[usedDoseIndex].dose += item.dose
+            nextMedicineIn24hour.push({medicine:medicin, time:item.time, dose:item.dose, dosesUntilNow:usedDose[usedDoseIndex].dose, canBeTaken:false})
+          }
+        }
+      })
+    });
+    nextMedicineIn24hour.sort((a, b) => (a.time > b.time) ? 1 : -1)
+    console.log("nextMedicineIn24hour:",nextMedicineIn24hour)
+    const grouped = {};
+    nextMedicineIn24hour.forEach((medicine) => {
+      if (!grouped[medicine.time.toISOString().slice(0, 16)]) {
+        grouped[medicine.time.toISOString().slice(0, 16)] = [];
+      }
+      grouped[medicine.time.toISOString().slice(0, 16)].push(medicine);
     });
 
     const groupedArray = Object.keys(grouped).map((timeKey) => ({
-      time: new Date(timeKey),
+      time:grouped[timeKey][0].time,
+      canBeTaken:grouped[timeKey][0].canBeTaken,
       medicines: grouped[timeKey]
     })).sort((a, b) => a.time - b.time);
-
+    console.log("grouped nextMedicineIn24hour:",groupedArray)
     return groupedArray;
-  };
-
-  const handleGroupCheck = (time) => {
-    setTakenStates(prevState => {
-      const updatedTakenStates = { ...prevState };
-      const updatedGroupedMedicines = groupedMedicines.map(group => {
-        if (group.time.getTime() === time.getTime()) {
-          return {
-            ...group,
-            medicines: group.medicines.map(medicine => {
-              if (!updatedTakenStates[medicine._id]) { // Only decrease count if not already taken
-                updatedTakenStates[medicine._id] = true;
-                medicine.history.push({
-                  startDate: new Date().toISOString(),
-                  endDate: new Date().toISOString(),
-                  state: "Active"
-                });
-                // Find the corresponding reminder for the specific time
-                medicine.reminder.forEach(reminder => {
-                  const { days, hours, minutes, dose } = getReminderTimes(reminder);
-                  if (days.includes(time.getDay()) && hours.includes(time.getHours()) && minutes.includes(time.getMinutes())) {
-                    medicine.count -= dose;
-                  }
-                });
-              }
-              return medicine;
-            })
-          };
-        }
-        return group;
-      });
-
-      const newGroupedMedicines = groupMedicinesByTime(filterMedicinesInNext48Hours(updatedGroupedMedicines.flatMap(group => group.medicines)));
-      setGroupedMedicines(newGroupedMedicines);
-
-      return updatedTakenStates;
-    });
-  };
+  }; 
 
   const getUnitName = (unitId) => {                   /*Neded*/
     const unit = Units?.find(u => u._id === unitId);
@@ -314,9 +231,7 @@ function Pilltaker() {
     const nextDose = getNextMedicineTime(medicine.reminder)[0]
     const isRefillNeeded = medicine.count < nextDose.dose;
     const canBeTaken = pillCanBeTaken(medicine)
-    const isTaken = takenStates[medicine._id];
-    console.log(`Rendering card for: ${medicine.name}, isRefillNeeded: ${isRefillNeeded}, isTaken: ${isTaken}`);
-    console.log("times",nextDose)
+    
     return (
       <Card 
         w='290px' 
@@ -367,7 +282,7 @@ function Pilltaker() {
               onClick={() => isRefillNeeded ? handlePillClick(medicine) : canBeTaken ? takePill() : null}
               style={{ width: '250px', height: '35px' }}
             >
-              {isRefillNeeded ? 'Refill Dose' : isTaken ? 'Take Medicine' : 'Next on '+ nextDose.time.toUTCString()}
+              {isRefillNeeded ? 'Refill Dose' : canBeTaken ? 'Take Medicine' : 'Next on '+ nextDose.time.toUTCString().slice(0, 22)}
             </Button>
           </Flex>
         </Flex>
@@ -375,14 +290,13 @@ function Pilltaker() {
     );
   });
 
-  // Render grouped medicines for the next 48 hours
-  const groupedPillCards = groupedMedicines.map((group, index) => {
-    const allTaken = group.medicines.every(medicine => takenStates[medicine._id]);
+  // Render grouped medicines for the next 24 hours
+  const groupedPillCards = groupedMedicines.map((group, index) => {   
     const localTime = new Date(group.time.getTime() - (group.time.getTimezoneOffset() * 60000));
     const dayOfWeek = localTime.toLocaleString('en-US', { weekday: 'long' });
     const date = localTime.toLocaleDateString('cs-CZ', { day: '2-digit', month: '2-digit', year: 'numeric' });
-    const time = localTime.toLocaleTimeString('cs-CZ', { hour: '2-digit', minute: '2-digit' });
-
+    const time = localTime.getUTCHours();
+    console.log("group:",group)
     return (
       <Card
         key={index}
@@ -393,38 +307,24 @@ function Pilltaker() {
         radius={10}
         p='10px'
         shadow="sm"
-        style={{ backgroundColor: allTaken ? theme.colors.gray[2] : 'white' }}
+        style={{ backgroundColor: group.canBeTaken ? 'white':theme.colors.gray[2]}}
       >
         <Group position="apart">
-          <Checkbox
-            p='10px'
-            checked={allTaken}
-            onChange={() => handleGroupCheck(group.time)}
-          />
-          <Title order={5}>{dayOfWeek}, {date} - {time}</Title>
+          <Title order={5}>{group.canBeTaken?"NOW":group.time.toUTCString().slice(0, 22)}</Title>
         </Group>
         {group.medicines.map((med, idx) => {
-          let dose = 0;
-          med.reminder.forEach(r => {
-            const { days, hours, minutes } = getReminderTimes(r);
-            if (days.includes(localTime.getDay()) && hours.includes(localTime.getHours()) && minutes.includes(localTime.getMinutes())) {
-              dose = r.dose; // Set correct dose
-            }
-          });
-
           return (
             <Group key={idx} style={{ marginLeft: '57px', marginTop: '5px' }}>
-              <Text
-                size="sm"
-                style={{
-                  textDecoration: takenStates[med._id] ? 'line-through' : 'none'
-                }}
-              >
-                {med.name} {dose} {getUnitName(med.unit)}
+              <Text size="sm">
+                {med.medicine.name} {med.dose} {getUnitName(med.medicine.unit)} {med.dosesUntilNow > med.medicine.count?'! (Need at least '+med.dosesUntilNow+' '+getUnitName(med.medicine.unit)+')':null}
               </Text>
             </Group>
           );
         })}
+        {group.canBeTaken ? <Button
+            p='10px'
+            onClick={() => /*handleGroupCheck(group.time)*/{}}
+          >Teke Medicine</Button>:null}
       </Card>
     );
   });
